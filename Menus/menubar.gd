bfa3c5523ff6c8df3ef55b783dae91e48264b604
layonthehorn <thomaspdressel@gmail.com>
2020-08-23 20:04:32 -0700

    Added scripts to test increase and decrease.

--- Menus/menubar.gd
+++ Menus/menubar.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+var number_points = 10
+var points_used = 0
+
 # opens the inventory
 func _on_Inventory_pressed():
 	print("Inventory")
@@ -15,3 +18,17 @@ func _on_Save_pressed():
 # quits the game
 func _on_Quit_pressed():
 	 get_tree().quit()
+
+
+func _on_Testing_Down_pressed():
+	if points_used > 0:
+		number_points += 1
+		points_used -= 1
+		print(points_used)
+
+
+func _on_Testing_Up_pressed():
+	if number_points > 0:
+		number_points -= 1
+		points_used += 1
+		print(points_used)
