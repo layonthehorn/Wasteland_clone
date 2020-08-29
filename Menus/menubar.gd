e76bc58e18e1ede7340d2ea26597145d2adea93f
layonthehorn <thomaspdressel@gmail.com>
2020-08-29 11:42:47 -0700

    asdj;flk

--- Menus/menubar.gd
+++ Menus/menubar.gd
@@ -18,17 +18,3 @@ func _on_Save_pressed():
 # quits the game
 func _on_Quit_pressed():
 	 get_tree().quit()
-
-
-func _on_Testing_Down_pressed():
-	if points_used > 0:
-		number_points += 1
-		points_used -= 1
-		print(points_used)
-
-
-func _on_Testing_Up_pressed():
-	if number_points > 0:
-		number_points -= 1
-		points_used += 1
-		print(points_used)
